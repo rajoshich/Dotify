6b732f15ded08600408ae38ced7936c3dbfb36e1
rajoshich <rajoshich@gmail.com>
2020-04-29 07:45:26 +0530

    updates

--- app/src/main/java/com/rajoshich/dotify/NowPlayingFragment.kt
+++ app/src/main/java/com/rajoshich/dotify/NowPlayingFragment.kt
@@ -5,11 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.ericchee.songdataprovider.Song
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 /**
  * A simple [Fragment] subclass.
  */
 class NowPlayingFragment : Fragment() {
+
+    private var song: Song? = null
+    companion object {
+        val TAG: String = NowPlayingFragment::class.java.simpleName
+        const val ARG_SONG = "arg_song"
+    }
+    private var randomNumber = Random.nextInt(200, 100000)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        playnumber.text = ("$randomNumber plays")
+
+        arguments?.let { args ->
+            val song = args.getParcelable<Song>(ARG_SONG)
+            if (song != null) {
+                this.song = song
+            }
+        }
+    }
+
+    fun updateSong(song :Song) {
+        this.song = song
+        updateSongView()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,4 +47,34 @@ class NowPlayingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_now_playing, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        updateSong()
+    }
+
+    private fun updateSongView() {
+
+        song?.let {
+            cover.setImageResource(it.largeImageID)
+            songName.text = it.title
+            artists.text = it.artist
+        }
+
+        play.setOnClickListener {
+            randomNumber++
+            playnumber.text = ("$randomNumber plays")
+        }
+    }
+
 }
+//if (song!= null) {
+//    song?.let {
+//        cover.setImageResource(it.largeImageID)
+//        songName.text = it.title
+//        artists.text = it.artist
+//    }
+//} else {
+//    Toast.makeText(context, "No song selected", Toast.LENGTH_LONG).show()
+//}
+//
--- app/src/main/java/com/rajoshich/dotify/activity/MainSongActivity.kt
+++ app/src/main/java/com/rajoshich/dotify/activity/MainSongActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ericchee.songdataprovider.Song
 import com.ericchee.songdataprovider.SongDataProvider
+import com.rajoshich.dotify.NowPlayingFragment
 import com.rajoshich.dotify.R
 import com.rajoshich.dotify.SongListFragment
 
@@ -14,15 +15,60 @@ class MainSongActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main_song)
 
         val allSongs: List<Song> = SongDataProvider.getAllSongs();
-        val songListFragment = SongListFragment()
+        val nowPlayingFragment = NowPlayingFragment()
+
         val argumentBundle = Bundle().apply {
-//            putParcelable(SongListFragment.ARG_SONG,
+            putParcelableArrayList(NowPlayingFragment.ARG_SONG, ArrayList(allSongs))
         }
 
+        nowPlayingFragment.arguments = argumentBundle
+
+        val songListFragment = SongListFragment()
 
         supportFragmentManager
             .beginTransaction()
             .add(R.id.fragmentContainer, songListFragment)
             .commit()
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            val hasBackStack = supportFragmentManager.backStackEntryCount > 0
+            if (hasBackStack) {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            } else {
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            }
+        }
     }
+
+//    Activity should implement the OnSongClickedListener
+//    i. When onSongClicked() is called, it should update the mini player information (similar
+//    behavior from hw2)
+//
+    private fun getSongDetailFragment() = supportFragmentManager.findFragmentByTag(NowPlayingFragment.TAG) as? NowPlayingFragment
+
+    override fun onSupportNavigateUp(): Boolean {
+        supportFragmentManager.popBackStack()
+        return super.onNavigateUp()
+    }
+
+    override fun onSongClicked(song: Song) {
+        var nowPlayingFragment = getSongDetailFragment()
+        if (nowPlayingFragment == null) {
+            nowPlayingFragment = NowPlayingFragment()
+            val argumentBundle = Bundle().apply {
+                putParcelable(NowPlayingFragment.ARG_SONG, song)
+            }
+            nowPlayingFragment.arguments = argumentBundle
+
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, nowPlayingFragment, NowPlayingFragment.TAG)
+                .addToBackStack(NowPlayingFragment.TAG)
+                .commit()
+        } else {
+            nowPlayingFragment.updateSong(song)
+        }
+    }
+
+
 }
